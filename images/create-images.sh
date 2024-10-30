896a3f14947e43c84871ec1c488a4002f0f97727
Pierre-Yves Chibon <pingou@pingoured.fr>
2024-10-30 17:20:04 +0100

    Make create-image.sh work with aib.sh
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- images/create-images.sh
+++ images/create-images.sh
@@ -10,26 +10,18 @@ target=$2
 mode=$3
 export=$4
 
-aib=$(mktemp -d --suffix=-aib)
 wd=$(pwd)
 build=$wd/_build
 ostree_repo=$wd/_ostree
 
-# Retrieve the tools we need
-git clone https://gitlab.com/CentOS/automotive/src/automotive-image-builder.git $aib
-
 # This doesn't quite seem to work atm
-#curl -o $aib/automotive-image-builder \
-    #"https://gitlab.com/CentOS/automotive/sample-images/-/raw/main/auto-image-builder.sh?inline=false"
+curl -o automotive-image-builder \
+    "https://gitlab.com/CentOS/automotive/sample-images/-/raw/main/auto-image-builder.sh?inline=false"
 
 # Build the base image
-# sudo sh $aib/automotive-image-builder \
-
-# Build the base image
-$aib/automotive-image-builder \
+sudo SHARE_PODMAN_MACHINE_ROOT="-v /var/lib/containers/storage:/var/lib/containers/storage -v $wd:$wd" \
+    sh -ex aib \
     --verbose \
-    --container \
-    --include=/var/lib/containers/storage/ \
     build \
     --distro $distro \
     --target $target \
@@ -43,5 +35,6 @@ $aib/automotive-image-builder \
     $wd/images/base.mpp.yml \
     $wd/$distro-$target-$mode.$(arch)-base.$export
 
-# /!\ "--include=/var/lib/containers/storage/" is required to have the
-# local container images available in the container
+
+# /!\ "SHARE_PODMAN_MACHINE_ROOT .. /var/lib/containers/storage/" is
+# required to have the local container images available in the container
